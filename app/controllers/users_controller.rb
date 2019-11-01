e40ca963da929f77dfc25cab2d0734c295d20f5f
ncooney94 <nick.cooney@gmail.com>
2019-11-02 10:36:11 +1100

    finished session controller and inluded current_user status in applicationcontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,17 @@ class UsersController < ApplicationController
     @user = User.new
   end
   def create
-    user = User.create params[:user]
-    redirect_to root_path
+    @user = User.new user_params
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   def user_params
-    params.requires(:user)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'products/index'
   get 'products/show'
+  get 'session/new'
   # get 'pages/home'
   root :to => 'pages#home'
   resources :users, :only => [:new, :create, :index]
